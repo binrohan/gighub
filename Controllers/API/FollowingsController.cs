4c0c0b7aa30113d9604afc08f0164b9d99bf0c2b
Bin Rohan <binrohan.cs@gmail.com>
2021-06-25 05:12:15 +0600

    Add following artist functionality

---
+++ Controllers/API/FollowingsController.cs
@@ -0,0 +1,41 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.API
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FolloweeId == dto.FolloweeId && f.FolloweeId == userId))
+            {
+                BadRequest("You are following the artist aleardy.");
+            }
+
+            var follow = new Following
+            {
+                FolloweeId = dto.FolloweeId,
+                FollowerId = userId
+            };
+
+            _context.Followings.Add(follow);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
